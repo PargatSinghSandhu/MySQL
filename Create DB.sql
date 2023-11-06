df9aeee85222ea4a60f719829267f7165131d9b0
Pargat Singh <79877236+PargatSinghSandhu@users.noreply.github.com>
2023-11-06 00:44:39 -0700

    Create Create DB.sql

---
+++ Create DB.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE parsisan_record_company;
+
+USE parsisan_record_company;
+
+CREATE TABLE bands ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, PRIMARY KEY (id));
+
+CREATE TABLE albums ( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, release_year INT, band_id INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (band_id) REFERENCES (id));
